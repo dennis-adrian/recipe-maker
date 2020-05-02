066c25f605bcdbf4619e2dfa37813cacb5cff1d8
Dennis Adrian <adriangbetancur@gmail.com>
2020-05-02 16:17:12 -0400

    Se agrego las funciones makeRecipe() y viewRecipe()

--- src/Main.kt
+++ src/Main.kt
@@ -12,13 +12,35 @@ fun main() {
             3. Salir
         """.trimIndent()
     try {
-        do {
+        do{
             println(menu)
             val response: String? = readLine()
-        } while (response?.toInt() != 3)
+            when(response?.toInt()){
+                1 -> println(makeRecipe())
+                2 -> println(viewRecipe())
+                3 -> println("Hasta la Proxima!!!")
+                else -> println("Elige una opcion valida")
+            }
+        }while (response?.toInt() != 3)
     } catch (e: NumberFormatException){
         println("Tiene que ingresar un numero")
     }
-
-
+}
+fun makeRecipe() : String {
+    val ingredients: String = """
+        Hacer receta
+        Selecciona por categoría el ingrediente que buscas
+        1. Agua
+        2. Leche
+        3. Carne
+        4. Verduras
+        5. Frutas
+        6. Cereal
+        7. Huevos
+        8. Aceites
+    """.trimIndent()
+    return ingredients
+}
+fun viewRecipe(): String {
+    return "Ver mis recetas"
 }
